ae35f88298ab6e2ee50cd84d620039849b10fff5
Mohammad Mahdi Mottaghi <mottaghimohammadmahdi@gmail.com>
2023-05-17 13:56:36 +0330

    edit Question component UI

--- Quiz App/src/components/Question/index.tsx
+++ Quiz App/src/components/Question/index.tsx
@@ -19,12 +19,12 @@ export const Question = function () {
   console.log(state[pageCounter].correct_answer);
   //   console.log(answers.sort());
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white flex flex-col  gap-8 px-7 py-10 w-2/3 rounded">
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white flex flex-col justify-center gap-8 px-7 py-10 w-2/3 h-[550px] rounded">
       <div className="text-end text-green-500">
         Correct Answers: {correctAnswer}/{state.length}
       </div>
       <div className="text-center flex flex-col items-center justify-center gap-5">
-        <h2 className=" text-3xl font-bold">{state[pageCounter].question}</h2>
+        <h2 className="h-[120px] text-3xl font-bold">{state[pageCounter].question}</h2>
         <div className="flex flex-col gap-3 w-[80%] [&_>_*]:w-full [&_>_*]:bg-blue-500 [&_>_*]:py-2 [&_>_*]:rounded">
           {answers.sort().map((item) => {
             return <div key={item}>{item}</div>;
